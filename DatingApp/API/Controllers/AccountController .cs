d5f5eb263c4fe3c6d7a349d3512f59862b650965
Malek Butto <malekbutto@gmail.com>
2022-10-17 02:40:28 +0300

    39. Adding a login endpoint

--- DatingApp/API/Controllers/AccountController .cs
+++ DatingApp/API/Controllers/AccountController .cs
@@ -17,7 +17,7 @@ namespace API.Controllers
         }
 
         [HttpPost("Register")]  //POST api/account/register
-        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        public async Task<ActionResult<AppUser>> Register(LoginDto registerDto)
         {
             if (await UserExists(registerDto.UserName)) return BadRequest("Username is taken"); 
 
@@ -35,6 +35,29 @@ namespace API.Controllers
             
             return user;
         }
+
+        [HttpPost("Login")]  //POST api/account/login
+        public async Task<ActionResult<AppUser>> Login(RegisterDto loginDto)
+        {
+            // get user from db
+            var user = await this._context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.UserName.ToLower());
+            if (user == null) return Unauthorized("Invalid username or password");
+
+            // check password ... so using hmac to do the reverse of what we did to register
+            // calculate the hash using the salt and the given password
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            var computeHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computeHash.Length; i++)
+            {
+                if (computeHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid username or password");
+            }
+
+            return user;
+        
+        }
+
 
     private async Task<bool> UserExists(string username)
     {
