ee1ce7d272ee99963c68df068cab6defc35f6f3c
Malek Butto <malekbutto@gmail.com>
2022-10-18 02:01:25 +0300

    41. Adding a token service

--- DatingApp/API/Startup.cs
+++ DatingApp/API/Startup.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
+using API.Interfaces;
+using API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -28,6 +30,12 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // 3 types of services lifetimes after we start it:
+            //AdSingleton: dies (disposed) with the application (too long for service we need on the call level) 
+            //AddScoped: dies (disposed) with the http request (in this case its scoped to the request , we create it on http call (injected to the controller), most useful in Web Apps)
+            //AddTransient: dies (disposed) on method finishing, creates every time they are injected or requested.
+
+            services.AddScoped<ITokenService, TokenService>();
             services.AddDbContext<DataContext>(optiones => {
                 optiones.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
